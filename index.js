73b41d96fdbaa0c643237a6233273d8d9ece1408
ShotRep <78008209+ShotRep@users.noreply.github.com>
2021-08-10 09:27:34 -0500

    Installation of chalk

--- index.js
+++ index.js
@@ -1,12 +1,42 @@
 #!/usr/bin/env node
-//allows us to execute node directly with cmd npm link
 
-const fs = require('fs')
+// Method #1
+// const lstat = filename => {
+//   return new Promise((resolve, reject) => {
+//     fs.lstat(filename, (err, stats) => {
+//       if (err) {
+//         reject(err);
+//       }
+//       resolve(stats);      
+//     });    
+//   });  
+// };
 
-fs.readdir(process.cwd(), (error, filenames) => {
-  if (error) {
-     console.log(error)
-    throw new Error(error)   
+const fs = require("fs")
+const util = require("util")
+
+// // Method #2
+// const lstat = util.promisify(fs.lstat);
+
+// Method #3
+const {lstat} = fs.promises
+
+fs.readdir(process.cwd(), async (err, filenames) => {
+  if (err) {
+    console.log(err)
   }
-  console.log(filenames)
+
+  const statPromises = filenames.map((filename) => {
+    return lstat(filename)
+  })
+
+  const allStats = await Promise.all(statPromises)
+
+  for (let stats of allStats) {
+    const index = allStats.indexOf(stats)
+
+    console.log(filenames[index], stats.isFile())
+  }
 })
+
+
